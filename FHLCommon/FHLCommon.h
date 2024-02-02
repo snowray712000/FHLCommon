cb4721553d324c0901d5e7600a8610b196ea91fc
EmmanuelLo <snowray712000@gmail.com>
2024-02-02 13:44:00 +0800

    Initial Commit

---
+++ FHLCommon/FHLCommon.h
@@ -0,0 +1,18 @@
+//
+//  FHLCommon.h
+//  FHLCommon
+//
+//  Created by littlesnow on 2024/2/2.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FHLCommon.
+FOUNDATION_EXPORT double FHLCommonVersionNumber;
+
+//! Project version string for FHLCommon.
+FOUNDATION_EXPORT const unsigned char FHLCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FHLCommon/PublicHeader.h>
+
+
